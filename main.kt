e7ac67b0b5ea8762c92a189bc3802883047ae6d5
Utkarsh Trivedi <143016878+ututkarsh22@users.noreply.github.com>
2025-07-14 16:11:12 +0530

    Delete main.kt

--- main.kt
+++
@@ -1,7 +0,0 @@
-fun main() {
-    val t = readLine()!!.toInt()
-    repeat(t) {
-        val (x, y) = readLine()!!.split(" ").map { it.toInt() }
-        println("${minOf(x, y)} ${maxOf(x, y)}")
-    }
-}
